108dcace4deb2e8e54e14002a197bcf004a51247
tkal00 <timo.kaljazin@hotmail.com>
2024-08-10 22:10:53 +0200

    cmake: directx shader compilation and copying

--- Dsemi/dsemi/graphics/directx/rendertargetview_dx11.cpp
+++ Dsemi/dsemi/graphics/directx/rendertargetview_dx11.cpp
@@ -1,10 +1,2 @@
-#include <dsemi/graphics/rendertargetview.h>
 #include <dsemi/graphics/swapchain.h>
 
-dsemi::graphics::RenderTargetView::RenderTargetView(RenderTarget* renderTarget)
-{
-}
-
-dsemi::graphics::RenderTargetView::~RenderTargetView()
-{
-}
--- Dsemi/dsemi/graphics/shaders/directx/default_ps.hlsl
+++ Dsemi/dsemi/graphics/shaders/directx/default_ps.hlsl
@@ -4,5 +4,5 @@ struct ps_in {
 
 float4 main(ps_in input) : SV_TARGET
 {
-	return float4(0.8f, 0.3f, 1.0f, 1.0f);
+	return float4(1.0f, 1.0f, 1.0f, 1.0f);
 }
